84389c0ef08ad360da0497943de1942fad63346f
Joabaluga <flavio007jr@hotmail.com>
2019-07-02 20:19:48 -0300

    Entity Framewor y Required

--- Tarea/Entities/Doctor.cs
+++ Tarea/Entities/Doctor.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    class Doctor
+     public class Doctor
     {
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Specialties Specialty { get; set; }
--- Tarea/Entities/Hospital.cs
+++ Tarea/Entities/Hospital.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,9 @@ namespace Entities
 {
     public class Hospital
     {
-
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Address { get; set; }
 
