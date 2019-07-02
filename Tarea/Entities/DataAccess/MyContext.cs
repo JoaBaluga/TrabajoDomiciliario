ca9663b920618928cf1836cb2fb9aba4c6a7b097
Joabaluga <flavio007jr@hotmail.com>
2019-07-02 20:51:17 -0300

    Metodos LinQ

---
+++ Tarea/Entities/DataAccess/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System.Data.Entity;
+
+namespace DataAccess
+{
+    public class MyContext : DbContext
+    {
+
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public MyContext() : base("name=ActualConnectionString")
+        {
+        }
+    }
+}
---
+++ Tarea/Entities/Repositories/DoctorsRepositories.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class DoctorsRepositories
+    {
+        public Doctor GetOlderDoctorWorking()
+        {
+            using (MyContext context = new MyContext())
+            {
+                return context.Doctors.Where(d => d.Hospitals.Count > 0).OrderByDescending(d => d.DateOfBirth).FirstOrDefault();
+            }
+            
+        }
+
+        public List<Doctor> GetOlderDoctorsWithSpecialties()
+        {
+            using (MyContext context = new MyContext())
+            {
+                return context.Doctors.Where(d => (d.Specialty == Specialties.Pediatrics) || (d.Specialty == Specialties.Fisiatry)).ToList();
+            }
+        }
+    }
+}
