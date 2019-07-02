7d5f9b9234724841d557c584bd1cbe87100fbbbc
Joabaluga <flavio007jr@hotmail.com>
2019-07-02 20:16:05 -0300

    Creacion Proyecto con Entidades

---
+++ Tarea/Entities/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Doctor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Specialties Specialty { get; set; }
+    }
+}
